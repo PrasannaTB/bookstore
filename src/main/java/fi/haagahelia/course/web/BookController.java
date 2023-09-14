8e1958af85bd1808d57bc30d74be1b9b9591482e
PrasannaTB <bgz905@myy.haaga-helia.fi>
2023-09-14 15:10:29 +0300

    Bookstore week 1

--- src/main/java/fi/haagahelia/course/web/BookController.java
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -3,20 +3,22 @@ package fi.haagahelia.course.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.course.domain.*;
 
-import fi.haagahelia.course.domain.*
-;@Controller
+@Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository;
 
 	@RequestMapping("/booklist")
 	public String bookList(Model model) {
-		System.out.println("TEST");
+
 		model.addAttribute("books", repository.findAll());
 
 		return "list";
@@ -39,5 +41,27 @@ public class BookController {
 		repository.save(book);
 		return "redirect:booklist";
 	}
+
+	@RequestMapping(value = "/edit")
+	public String editBook(Long id, Model model) {
+		Book book = repository.findById(id).orElse(null);
+		if (book != null) {
+			model.addAttribute("book", book);
+			return "editbook";
+		} else {
+			// Handle the case where the book with the given ID doesn't exist
+			return "redirect:/booklist";
+		}
+	}
+
+	@PostMapping("/edit/{id}")
+	public String editBookSubmit(@PathVariable("id") Long bookId, @ModelAttribute Book editedBook) {
+
+		editedBook.setId(bookId);
+		repository.save(editedBook);
+		return "redirect:/booklist";
+	}
+
+	
 
 }
