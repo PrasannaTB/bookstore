647b4e8bf207575857947b4e2b30b3ec2682362c
PrasannaTB <bgz905@myy.haaga-helia.fi>
2023-09-21 13:15:43 +0300

    Week 3 assignment

--- src/main/java/fi/haagahelia/course/web/BookController.java
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -1,5 +1,8 @@
 package fi.haagahelia.course.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.course.domain.*;
 
@@ -26,6 +30,18 @@ public class BookController {
 
 		return "list";
 	}
+	
+	// RESTful service to get all books
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	// RESTful service to get book by id
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBooktRest(@PathVariable("id") Long id) {	
+    	return repository.findById(id);
+    } 
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
@@ -53,7 +69,6 @@ public class BookController {
 			model.addAttribute("book", book);
 			model.addAttribute("category", crepository.findAll());
 			
-
 			return "editbook";
 		} else {
 			// Handle the case where the book with the given ID doesn't exist
